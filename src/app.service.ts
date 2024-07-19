3f6f824674ed7a439fecf21a50fd10597794f3e1
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-07-19 19:25:53 +0100

    🎨: Improve structure / format of the code
    Refactor the code to improve readability and structure documentation

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'Hello! from Todo rest API!';
+        return `<div style="text-align:center;"><h1 style="font-family:Arial,sans-serif;">Hello! from Todo REST API<br>Made with &#10084;&#65039; by Michael</h1><p style="background-color:#f0f0f0; padding:10px; border-radius:5px; font-size:16px; font-weight:bold; color:#333333;"><a href="https://tsoc-nestjs.vercel.app/swagger" style="color:#0066cc; text-decoration:none;">Go to the Swagger Documentation</a></p></div>`;
     }
 }
